dcbfcfcb412d2ef37563f8ab4b27e55cf6918d14
lucean <lucean@users.noreply.github.com>
2026-03-10 11:45:01 +0000

    Initial implementation of the spaceship-jj section.

--- spaceship-section.plugin.zsh
+++ spaceship-section.plugin.zsh
@@ -1,55 +1,51 @@
 #
-# Foobar
+# jj
 #
-# Foobar is a supa-dupa cool tool for making you development easier.
-# Link: https://www.foobar.xyz
+# jj is a supa-dupa cool tool for making you development easier.
+# Link: https://www.jj.xyz
 
 # ------------------------------------------------------------------------------
 # Configuration
 # ------------------------------------------------------------------------------
 
-SPACESHIP_FOOBAR_SHOW="${SPACESHIP_FOOBAR_SHOW=true}"
-SPACESHIP_FOOBAR_ASYNC="${SPACESHIP_FOOBAR_ASYNC=true}"
-SPACESHIP_FOOBAR_PREFIX="${SPACESHIP_FOOBAR_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"}"
-SPACESHIP_FOOBAR_SUFFIX="${SPACESHIP_FOOBAR_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
-SPACESHIP_FOOBAR_SYMBOL="${SPACESHIP_FOOBAR_SYMBOL="😍 "}"
-SPACESHIP_FOOBAR_COLOR="${SPACESHIP_FOOBAR_COLOR="yellow"}"
+SPACESHIP_JJ_SHOW="${SPACESHIP_JJ_SHOW=true}"
+SPACESHIP_JJ_ASYNC="${SPACESHIP_JJ_ASYNC=true}"
+SPACESHIP_JJ_PREFIX="${SPACESHIP_JJ_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"}"
+SPACESHIP_JJ_SUFFIX="${SPACESHIP_JJ_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
+SPACESHIP_JJ_SYMBOL="${SPACESHIP_JJ_SYMBOL="🥋 "}"
+SPACESHIP_JJ_COLOR="${SPACESHIP_JJ_COLOR="yellow"}"
 
 # ------------------------------------------------------------------------------
 # Section
 # ------------------------------------------------------------------------------
 
-# Show foobar status
+# Show jj status
 # spaceship_ prefix before section's name is required!
 # Otherwise this section won't be loaded.
-spaceship_foobar() {
-  # If SPACESHIP_FOOBAR_SHOW is false, don't show foobar section
-  [[ $SPACESHIP_FOOBAR_SHOW == false ]] && return
+spaceship_jj() {
+  # If SPACESHIP_JJ_SHOW is false, don't show jj section
+  [[ $SPACESHIP_JJ_SHOW == false ]] && return
 
-  # Check if foobar command is available for execution
-  spaceship::exists foobar || return
+  # Check if jj command is available for execution
+  spaceship::exists jj || return
 
-  # Show foobar section only when there are foobar-specific files in current
-  # working directory.
+  jj root >/dev/null 2>&1 || return
 
-  # spaceship::upsearch utility helps finding files up in the directory tree.
-  local is_foobar_context="$(spaceship::upsearch foobar.conf)"
-  # Here glob qualifiers are used to check if files with specific extension are
-  # present in directory. Read more about them here:
-  # http://zsh.sourceforge.net/Doc/Release/Expansion.html
-  [[ -n "$is_foobar_context" || -n *.foo(#qN^/) || -n *.bar(#qN^/) ]] || return
+  local jj_info
+  jj_info="$(
+    jj --at-op=@ --ignore-working-copy --no-pager \
+      log -r @ --limit 1 --no-graph \
+      --template 'change_id.shortest(8) ++ if(description, " (" ++ description.first_line() ++ ")", "")' \
+      2>/dev/null
+  )"
 
-  local foobar_version="$(foobar --version)"
+  [[ -z "$jj_info" ]] && return
 
-  # Check if tool version is correct
-  [[ $foobar_version == "system" ]] && return
-
-  # Display foobar section
-  # spaceship::section utility composes sections. Flags are optional
+  # Display jj section
   spaceship::section::v4 \
-    --color "$SPACESHIP_FOOBAR_COLOR" \
-    --prefix "$SPACESHIP_FOOBAR_PREFIX" \
-    --suffix "$SPACESHIP_FOOBAR_SUFFIX" \
-    --symbol "$SPACESHIP_FOOBAR_SYMBOL" \
-    "$foobar_version"
+    --color "$SPACESHIP_JJ_COLOR" \
+    --prefix "$SPACESHIP_JJ_PREFIX" \
+    --suffix "$SPACESHIP_JJ_SUFFIX" \
+    --symbol "$SPACESHIP_JJ_SYMBOL" \
+    "$jj_info"
 }
