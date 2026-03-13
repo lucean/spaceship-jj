fc50bdb12e68ac77707d89e7826efde585ec8165
lucean <lucean@users.noreply.github.com>
2026-03-13 14:58:50 +0000

    Update tests and ci workflows. (#7)
    * Update README.md.
    
    * Update tests.
    
    * Update checkout action to v6.0.2.
    
    * Update test job.
    
    * Add shunit2
    
    * Add spaceship installation.
    
    * Set SPACESHIP_HOME for test run.
    
    * Typo
    
    * Link in the shunit2 scripts
    
    * Typo
    
    * Install Jujutsu.
    
    * Stricter regex handling and fixed redirects.

--- tests/spaceship-jj.test.zsh
+++ tests/spaceship-jj.test.zsh
@@ -8,12 +8,6 @@ SHUNIT_PARENT=$0
 # Use system Spaceship or fallback to Spaceship Docker on CI
 typeset -g SPACESHIP_ROOT="${SPACESHIP_ROOT:=/spaceship}"
 
-# Mocked tool CLI
-mocked_version="v1.0.0-mocked"
-foobar() {
-  echo "$mocked_version"
-}
-
 # ------------------------------------------------------------------------------
 # SHUNIT2 HOOKS
 # ------------------------------------------------------------------------------
@@ -27,12 +21,12 @@ oneTimeSetUp() {
   export TERM="xterm-256color"
 
   source "$SPACESHIP_ROOT/spaceship.zsh"
-  source "$(dirname $CWD)/spaceship-section.plugin.zsh"
+  source "$(dirname $CWD)/spaceship-jj.plugin.zsh"
 
   SPACESHIP_PROMPT_ASYNC=false
   SPACESHIP_PROMPT_FIRST_PREFIX_SHOW=true
   SPACESHIP_PROMPT_ADD_NEWLINE=false
-  SPACESHIP_PROMPT_ORDER=(foobar)
+  SPACESHIP_PROMPT_ORDER=(jj)
 
   echo "Spaceship version: $(spaceship --version)"
 }
@@ -48,25 +42,61 @@ oneTimeTearDown() {
 # TEST CASES
 # ------------------------------------------------------------------------------
 
-test_incorrect_env() {
+test_jj_no_jj_repo() {
   local expected=""
   local actual="$(spaceship::testkit::render_prompt)"
 
-  assertEquals "do not render system version" "$expected" "$actual"
+  assertEquals "render in dir with no jj repo" "$expected" "$actual"
 }
 
-test_mocked_version() {
+test_jj_dir_without_desc() {
   # Prepare the environment
-  touch $SHUNIT_TMPDIR/test.foo
+  jj git init >/dev/null 2>&1
 
-  local prefix="%{%B%}$SPACESHIP_JJ_PREFIX%{%b%}"
-  local content="%{%B%F{$SPACESHIP_JJ_COLOR}%}$SPACESHIP_JJ_SYMBOL$mocked_version%{%b%f%}"
-  local suffix="%{%B%}$SPACESHIP_JJ_SUFFIX%{%b%}"
-
-  local expected="$prefix$content$suffix"
   local actual="$(spaceship::testkit::render_prompt)"
+  local expanded="$(print -P -- "$actual")"
+  local raw_section_text="$(printf '%s' "$expanded" | sed -E $'s/\x1b\\[[0-9;]*[[:alpha:]]//g')"
 
-  assertEquals "render mocked version" "$expected" "$actual"
+  local pattern='^on 🥋 [a-z0-9]{8} $'
+
+  [[ "$raw_section_text" =~ "$pattern" ]] \
+    || fail "render in jj dir missing expected content: <$raw_section_text>"
+
+  [[ "$expanded" =~ $'\e\\[[0-9;]*33m🥋 [a-z0-9]{8}' ]] \
+    || fail "jj change id should be yellow: <$expanded>"
+
+  # Check that the prompt begins with bold 'on '
+  [[ "$expanded" =~ $'^\e\\[[0-9;]*1m(on )' ]] \
+    || fail "prompt prefix should be bold: <$expanded>"
+}
+
+test_jj_dir_with_desc() {
+  # Prepare the environment
+  jj desc -m "Init" > /dev/null 2>&1
+
+  local actual="$(spaceship::testkit::render_prompt)"
+  local expanded="$(print -P -- "$actual")"
+  local raw_section_text="$(printf '%s' "$expanded" | sed -E $'s/\x1b\\[[0-9;]*[[:alpha:]]//g')"
+
+  local pattern='^on 🥋 [a-z0-9]{8} \(Init\) $'
+
+  [[ "$raw_section_text" =~ "$pattern" ]] \
+    || fail "render in jj dir missing expected content: <$raw_section_text>"
+}
+
+test_jj_dir_added_file_status() {
+  # Prepare the environment
+  touch new_file
+  jj file track new_file > /dev/null 2>&1
+
+  local actual="$(spaceship::testkit::render_prompt)"
+  local expanded="$(print -P -- "$actual")"
+  local raw_section_text="$(printf '%s' "$expanded" | sed -E $'s/\x1b\\[[0-9;]*[[:alpha:]]//g')"
+
+  local pattern='^on 🥋 [a-z0-9]{8} \(Init\) \[\+\] $'
+
+  [[ "$raw_section_text" =~ $pattern ]] \
+    || fail "render in jj dir with pattern: <$pattern>, but was <$raw_section_text>"
 }
 
 # ------------------------------------------------------------------------------
